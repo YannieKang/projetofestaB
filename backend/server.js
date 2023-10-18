7c2c61dbfdc08646cf6255d3c6814729b4475bed
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-18 09:32:39 -0300

    projeto festa 2mib

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,35 @@
-//modulos
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+//modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 //routes
 const authRouter = require("./routes/authRoutes");
-
-//midllewares
-
+//middlewares - travas do meio de projeto e acesso
 //config
 const dbName = "partytime";
 const port = 3000;
 const app = express();
-
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.static("public"));
-
-//atrelando a primeira rota
-app.use("/api/auth");
-
-//cibexao mongodb
+//atrelar as rotas ao express
+app.use("/api/auth", authRouter);
+//conexão mongodb
 mongoose.connect(
-    `mongodb://127.0.0.1:27017/${dbName}`, {
-    useNewUrlParser : true,
-    UseUnifiedTopology : true,
-    serverSelectionTimeoutMS : 20000
-    })
-
-    
-
-app.get("/", (reo, res)=>{
-    res.json({message : "Rota de teste, será trocada"})
-})
-
-//escutar ou entende a porta 3000
+`mongodb://127.0.0.1:27017/${dbName}`, {
+useNewUrlParser : true,
+UseUnifiedTopology : true,
+serverSelectionTimeoutMS : 10000
+}
+)
+app.get("/", (req, res)=> {
+//primeira rota de teste
+res.json({message : "Rota de teste será trocada!!!"});
+});
+//escutando a porta
 app.listen(port, ()=>{
-    console.log(`O backend está rodando na porta ${port}`)
-    });
+console.log(`O backend está rodando na porta ${port}`)
+});
     
 
